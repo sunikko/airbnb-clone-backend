cfea1da0a0d13e323417d85e2b06aaf3007eb3a0
Soy <soyeon.won@oxbridgeuk.com>
2024-08-22 11:27:56 +0100

    [rest_frmaework] is_valid:

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import category
@@ -10,12 +11,19 @@ def categories(request):
         all_categories = category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
-        category.objects.create(
-            name=request.data["name"],
-            kind=request.data["kind"],
-        )
-        return Response({"created": True})
+    elif request.method == "POST":        
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            # category.objects.create(
+            #     name=request.data["name"],
+            #     kind=request.data["kind"],
+            # )
+            if 'pk' in request.data:
+                return Response({"error": "The 'pk' field should not be included in the request."}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({"created": True})
+        else:
+            return Response(serializer.errors)
 
 @api_view()
 def categoryDetails(reqest, pk):
