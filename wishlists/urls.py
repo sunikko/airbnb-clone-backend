c99f54ec544b3d2ac1be132b262d404ffbbe648a
Soy <soyeon.won@oxbridgeuk.com>
2024-08-26 23:38:31 +0100

    WishlistDetail:

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Wishlists
+from .views import Wishlists, WishlistDetail
 
 urlpatterns = [
     path("", Wishlists.as_view()),
+    path("<int:pk>", WishlistDetail.as_view()),
 ]
--- wishlists/views.py
+++ wishlists/views.py
@@ -1,4 +1,6 @@
 from rest_framework.views import APIView
+from rest_framework.status import HTTP_200_OK
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Wishlist
@@ -26,6 +28,47 @@ class Wishlists(APIView):
                 user=request.user,
             )
             serializer = WishlistSerializer(wishlist)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
+
+class WishlistDetail(APIView):
+    
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk, user):
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        serializer = WishlistSerializer(
+            wishlist,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        wishlist.delete()
+        return Response(status=HTTP_200_OK)
+
+    def put(self, request, pk):
+        wishlist = self.get_object(pk, request.user)
+        serializer = WishlistSerializer(
+            wishlist,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            wishlist = serializer.save()
+            serializer = WishlistSerializer(
+                wishlist,
+                context={"request": request},
+            )
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
