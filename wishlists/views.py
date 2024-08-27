e4c9de8995ba960e50987d1e18a59358be93e334
Soy <soyeon.won@oxbridgeuk.com>
2024-08-27 11:05:13 +0100

    WishlistToggle:

--- wishlists/views.py
+++ wishlists/views.py
@@ -3,6 +3,7 @@ from rest_framework.status import HTTP_200_OK
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rooms.models import Room
 from .models import Wishlist
 from .serializers import WishlistSerializer
 
@@ -72,3 +73,27 @@ class WishlistDetail(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+        
+
+
+class WishlistToggle(APIView):
+    def get_list(self, pk, user):
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get_room(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def put(self, request, pk, room_pk):
+        wishlist = self.get_list(pk, request.user)
+        room = self.get_room(room_pk)
+        if wishlist.rooms.filter(pk=room.pk).exists():
+            wishlist.rooms.remove(room)
+        else:
+            wishlist.rooms.add(room)
+        return Response(status=HTTP_200_OK)
