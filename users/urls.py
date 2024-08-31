54cfc1ab71b93dd93530457a223bc8cfd047638e
Soy <soyeon.won@oxbridgeuk.com>
2024-08-31 14:17:40 +0100

    users log-in log-out:

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("myprofile", views.Myprofile.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
     path("change-password", views.ChangePassword.as_view()),
+    path("log-in", views.LogIn.as_view()),
+    path("log-out", views.LogOut.as_view()),
 ]
